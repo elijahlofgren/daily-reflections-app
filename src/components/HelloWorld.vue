ba4cdc2658baa472273c64162548d3c86dcb6568
Elijah Lofgren <elijahlofgren@gmail.com>
2018-05-19 13:21:35 -0500

    Add links to lat34north.com and waymarking.com

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -18,7 +18,7 @@ export default {
           Coordinates: ['33.45123333', '-86.71606666'],
           County: 'Jackson',
           Title: 'Anthony L. Barnes/The Cahaba Pumping Station',
-          Lat34Key: '37-175',
+          LatKey: '37-175',
           Waymark: 'WMKTM7',
         },
       ],
@@ -28,13 +28,16 @@ export default {
     // https://www.google.com/maps/?q=32.37685,-86.30078333
     const gmapUrlPrefix = 'https://www.google.com/maps/?q=';
     const hereMapsUrlPrefix = 'https://wego.here.com/search/';
+    const waymarkUrlPrefix = 'http://www.waymarking.com/waymarks/';
+    const latUrlPrefix = 'http://www.lat34north.com/HistoricMarkersAL/MarkerDetail.cfm?KeyID=';
     for (let i = 0; i < preppedData.length; i += 1) {
       const marker = preppedData[i];
       if (marker.Coordinates && marker.Coordinates.length > 1) {
         // Map URLS
         preppedData[i].gmapsUrl = `${gmapUrlPrefix}${marker.Coordinates[0]},${marker.Coordinates[1]}`;
-
         preppedData[i].hereMapsUrl = `${hereMapsUrlPrefix}${marker.Coordinates[0]},${marker.Coordinates[1]}`;
+        preppedData[i].waymarkUrl = `${waymarkUrlPrefix}${marker.Waymark}`;
+        preppedData[i].latUrl = `${latUrlPrefix}${marker.LatKey}`;
       }
     }
 
@@ -53,8 +56,10 @@ export default {
     <li v-for='(marker,index) of markers' :key='index'>
       <p><strong>title: {{marker.Title}}</strong></p>
       <p>{{marker.County}}</p>
-      <a :href='marker.hereMapsUrl'>Directions (Here Map)</a>
-      <a :href='marker.gmapsUrl'>Directions (Google Map)</a>
+      <a :href='marker.hereMapsUrl'>Directions (Here Map)</a><br />
+      <a :href='marker.gmapsUrl'>Directions (Google Map)</a><br />
+      <a :href='marker.waymarkUrl'>Waymark</a><br />
+      <a :href='marker.latUrl'>Description of marker</a>
     </li>
   </ul>
 
