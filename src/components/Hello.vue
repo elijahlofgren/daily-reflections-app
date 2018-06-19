78553b9d98236020440e1003b2970fc3af6efe35
Elijah Lofgren <elijahlofgren@gmail.com>
2018-06-19 17:46:27 -0500

    Add flag to use hard coded GPS ( ((useful when developing in desktop browser)

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -59,9 +59,79 @@ function distanceTo(point1, point2) {
   return rounded;
 }
 // End adapted from https://github.com/chrisveness/geodesy/blob/master/latlon-spherical.js
-
+const useHardCodedGps = true;
 let userLat = 32.37685;
 let userLon = -86.30078333;
+
+/* eslint-enable */
+const MarkersData = {
+  markers: [
+    {
+      City: 'Vestavia',
+      Coordinates: [33.45123333, -86.71606666],
+      County: 'Jackson',
+      Title: 'Anthony L. Barnes/The Cahaba Pumping Station',
+      LatKey: '37-175',
+      Waymark: 'WMKTM7',
+    },
+    {
+      City: 'Montgomery',
+      Coordinates: [32.37685, -86.30078333],
+      County: 'Montgomery',
+      Title: 'Alabama Highway Patrol',
+      LatKey: 'tbd',
+      Waymark: 'tbd',
+    },
+    {
+      City: 'Headland',
+      Coordinates: [31.35148333, -85.33825],
+      County: 'Henry',
+      Title: 'First Baptist Church of Headland',
+      LatKey: 'tbd',
+      Waymark: 'tbd',
+    },
+    {
+      City: 'Screamer Community',
+      Coordinates: [31.68158333, -85.2021],
+      County: 'Henry',
+      Title: 'Liberty United Methodist Church / Hilliardsville',
+      LatKey: 'tbd',
+      Waymark: 'tbd',
+    },
+    {
+      City: 'Fort Mitchell',
+      Coordinates: [32.3441, -85.021],
+      County: 'Russell',
+      Title: 'Indian Ball Ground',
+      LatKey: 'tbd',
+      Waymark: 'tbd',
+    },
+    {
+      City: 'Holy Trinity',
+      Coordinates: [32.22061666, -85.00588333],
+      County: 'Russell',
+      Title: 'Spanish Fort',
+      LatKey: 'tbd',
+      Waymark: 'tbd',
+    },
+    {
+      City: 'Highland Home',
+      Coordinates: [31.95226666, -86.3137],
+      County: 'Crenshaw',
+      Title: 'Site of Highland Home College',
+      LatKey: 'tbd',
+      Waymark: 'tbd',
+    },
+    {
+      City: 'Birmingham',
+      Coordinates: [33.5704, -86.7248],
+      County: 'Jefferson',
+      Title: 'East Lake Park',
+      LatKey: 'tbd',
+      Waymark: 'tbd',
+    },
+  ],
+};
 
 export default {
   name: 'HelloWorld',
@@ -74,119 +144,58 @@ export default {
   created() {
     const vm = this;
 
-    /* eslint-enable */
-    const MarkersData = {
-      markers: [
-        {
-          City: 'Vestavia',
-          Coordinates: [33.45123333, -86.71606666],
-          County: 'Jackson',
-          Title: 'Anthony L. Barnes/The Cahaba Pumping Station',
-          LatKey: '37-175',
-          Waymark: 'WMKTM7',
-        },
-        {
-          City: 'Montgomery',
-          Coordinates: [32.37685, -86.30078333],
-          County: 'Montgomery',
-          Title: 'Alabama Highway Patrol',
-          LatKey: 'tbd',
-          Waymark: 'tbd',
-        },
-        {
-          City: 'Headland',
-          Coordinates: [31.35148333, -85.33825],
-          County: 'Henry',
-          Title: 'First Baptist Church of Headland',
-          LatKey: 'tbd',
-          Waymark: 'tbd',
-        },
-        {
-          City: 'Screamer Community',
-          Coordinates: [31.68158333, -85.2021],
-          County: 'Henry',
-          Title: 'Liberty United Methodist Church / Hilliardsville',
-          LatKey: 'tbd',
-          Waymark: 'tbd',
-        },
-        {
-          City: 'Fort Mitchell',
-          Coordinates: [32.3441, -85.021],
-          County: 'Russell',
-          Title: 'Indian Ball Ground',
-          LatKey: 'tbd',
-          Waymark: 'tbd',
-        },
-        {
-          City: 'Holy Trinity',
-          Coordinates: [32.22061666, -85.00588333],
-          County: 'Russell',
-          Title: 'Spanish Fort',
-          LatKey: 'tbd',
-          Waymark: 'tbd',
-        },
-        {
-          City: 'Highland Home',
-          Coordinates: [31.95226666, -86.3137],
-          County: 'Crenshaw',
-          Title: 'Site of Highland Home College',
-          LatKey: 'tbd',
-          Waymark: 'tbd',
-        },
-        {
-          City: 'Birmingham',
-          Coordinates: [33.5704, -86.7248],
-          County: 'Jefferson',
-          Title: 'East Lake Park',
-          LatKey: 'tbd',
-          Waymark: 'tbd',
-        },
-      ],
-    };
-
+    if (useHardCodedGps) {
+      vm.gpsReady();
+    } else {
     /* eslint-disable */
     navigator.geolocation.getCurrentPosition(
       function success(position) {
         userLat = position.coords.latitude;
         userLon = position.coords.longitude;
         //alert('Latitude: ' + position.coords.latitude + ' Longitude: ' + position.coords.longitude);
-
-          const preppedData = MarkersData.markers;
-        // URL like
-        // https://www.google.com/maps/?q=32.37685,-86.30078333
-        const gmapUrlPrefix = 'https://www.google.com/maps/?q=';
-        const hereMapsUrlPrefix = 'https://wego.here.com/search/';
-        const waymarkUrlPrefix = 'http://www.waymarking.com/waymarks/';
-        const latUrlPrefix = 'http://www.lat34north.com/HistoricMarkersAL/MarkerDetail.cfm?KeyID=';
-        for (let i = 0; i < preppedData.length; i += 1) {
-          const marker = preppedData[i];
-          if (marker.Coordinates && marker.Coordinates.length > 1) {
-            // Map URLS
-            preppedData[i].gmapsUrl = `${gmapUrlPrefix}${marker.Coordinates[0]},${marker.Coordinates[1]}`;
-            preppedData[i].hereMapsUrl = `${hereMapsUrlPrefix}${marker.Coordinates[0]},${marker.Coordinates[1]}`;
-            preppedData[i].waymarkUrl = `${waymarkUrlPrefix}${marker.Waymark}`;
-            preppedData[i].latUrl = `${latUrlPrefix}${marker.LatKey}`;
-
-            // Measure between two points:
-            const lat = preppedData[i].Coordinates[0];
-            const lon = preppedData[i].Coordinates[1];
-            const result = distanceTo(
-              { lat, lon },
-              { lat: userLat, lon: userLon },
-            );
-            preppedData[i].distance = result;
-          }
-        }
-
-        // console.log(JSON.stringify(preppedData));
-
-        // JSON responses are automatically parsed.
-        vm.markers = preppedData;
+        vm.gpsReady();
       },
         function error(error) {
           alert('code: ' + error.code + '\n' + 'message: ' + error.message + '\n');
         },
       );
+    }
+  },
+  methods: {
+    gpsReady() {
+      let vm = this;
+      const preppedData = MarkersData.markers;
+      // URL like
+      // https://www.google.com/maps/?q=32.37685,-86.30078333
+      const gmapUrlPrefix = 'https://www.google.com/maps/?q=';
+      const hereMapsUrlPrefix = 'https://wego.here.com/search/';
+      const waymarkUrlPrefix = 'http://www.waymarking.com/waymarks/';
+      const latUrlPrefix = 'http://www.lat34north.com/HistoricMarkersAL/MarkerDetail.cfm?KeyID=';
+      for (let i = 0; i < preppedData.length; i += 1) {
+        const marker = preppedData[i];
+        if (marker.Coordinates && marker.Coordinates.length > 1) {
+          // Map URLS
+          preppedData[i].gmapsUrl = `${gmapUrlPrefix}${marker.Coordinates[0]},${marker.Coordinates[1]}`;
+          preppedData[i].hereMapsUrl = `${hereMapsUrlPrefix}${marker.Coordinates[0]},${marker.Coordinates[1]}`;
+          preppedData[i].waymarkUrl = `${waymarkUrlPrefix}${marker.Waymark}`;
+          preppedData[i].latUrl = `${latUrlPrefix}${marker.LatKey}`;
+
+          // Measure between two points:
+          const lat = preppedData[i].Coordinates[0];
+          const lon = preppedData[i].Coordinates[1];
+          const result = distanceTo(
+            { lat, lon },
+            { lat: userLat, lon: userLon },
+          );
+          preppedData[i].distance = result;
+        }
+      }
+
+        // console.log(JSON.stringify(preppedData));
+
+        // JSON responses are automatically parsed.
+        vm.markers = preppedData;
+    }
   },
   mounted() {
     const mymap = L.map('mapid').setView([userLat, userLon], 15);
